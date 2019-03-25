83b82d0083c3330cd61123275df7542f63b737b1
kleranscoding <clarencecc88@gmail.com>
2019-03-25 15:49:11 -0500

    restyled navbar, added more logic

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -5,14 +5,36 @@ import axios from 'axios';
 import store from '../store';
 import { setCurrentUser } from  '../actions/auth';
 
-
+const backURL = 'http://localhost:8088'
 class Profile extends React.Component {
+
+  state = {
+    username: "",
+    saved_texts: [],
+  };
 
   componentWillMount () {
     if (!localStorage.token) {
       console.log("no token! redirecting...")
       this.props.history.push("/");
-    }
+      return;
+    } 
+
+    const config = {
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("token",""),
+        "Content-Type": "application/json",
+      }
+    };
+
+    axios.get(backURL+'/api/users/profile',{},config)
+    .then(res => {
+      console.log(res.data)
+
+    })
+    .catch(err => {
+      console.log(err.response);
+    });
   }
   
   render() {
